df63ecacd1f2aba5cb2f06d8b5ffbddc45bc5fd6
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-09-17 22:42:05 +0200

    Add files via upload

---
+++ ej3s.php
@@ -0,0 +1,14 @@
+<HTML>
+<HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
+<BODY>
+<?php
+	$ip="192.18.16.204";
+	$mascara=16;
+	echo "IP: $ip <br>";
+	echo "Mascara: $mascara <br>";
+	echo "Direccion Red: 192.18.0.0 <br>";
+	echo "Direccion Broadcast: 192.18.255.255 <br>";
+	echo "Rango 192.18.0.1 a 192.18.255.254 <br>";
+?>
+</BODY>
+</HTML>
