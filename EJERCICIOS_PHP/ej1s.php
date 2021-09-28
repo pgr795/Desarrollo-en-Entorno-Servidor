3824fb88827229b4b64a37b87ef9faa721f3c6bb
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-09-28 18:13:20 +0200

    Delete EJERCICIOS_PHP directory

--- EJERCICIOS_PHP/ej1s.php
+++
@@ -1,9 +0,0 @@
-<HTML>
-<HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
-<BODY>
-<?php
-	$ip="192"."18"."16"."204";
-	echo "IP 192.18.16.204 en binario es "; printf("%b",$ip);
-?>
-</BODY>
-</HTML>
