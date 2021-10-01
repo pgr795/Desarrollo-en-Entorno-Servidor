c98f4a2a02258a9651f992a5b07c5581d35be9d6
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-10-01 08:29:02 +0200

    Add files via upload

--- PHP/Ejercicios/ej10s.php
+++ PHP/Ejercicios/ej10s.php
@@ -1,34 +1,52 @@
 <HTML> 
 	<HEAD>
-		<TITLE> EJ1B – Conversor decimal a binario</TITLE>
+		<TITLE> EJ10</TITLE>
 	</HEAD>  
 	<BODY> 
 		<?php  
 			$var=array();
 			$numero=1;
-			$resultado=0;
-				echo "<table border=1px>";
+				/* echo "<table border=1px>";
 				echo "<tr></tr>";
 				echo "<td>Indice</td>
 					  <td>Valor</td>
 					  <td>Suma</td>";
-				for($i=1;$i<20;$i++){
+					   */
+
+			for($i=0;$i<20;$i++)
+			{
+				$var[$i]=$numero;
+				$numero=$numero+2;	
+				echo "$i";
+				if($var[$i]%2==0){
+					
+					
+				}
+			}	
+					
+			//print_r($var);	
+			var_dump($var);	   
+					   
+					   
+					   
+					   
+				/* for($i=0;$i<20;$i++){
+					$var[$i]=;
+		
 					if($numero%2==0){
-						$i++;	
+						$numero++;
 					}
 					else{
 						$resultado=$resultado+$numero;
-						$numero=$i;
-						$var[$i]=$resultado;
 						$numero++;
 						
 						echo "<tr></tr>";
 							echo "<td>$i</td>
-								  <td>$numero</td>
-								  <td>$var[$i]</td>";
-					}	
-				}
-			echo "</table>";
+								  <td>$var[$i]</td>
+								  <td>$numero</td>";
+					}	 */				
+				
+			/* echo "</table>"; */
 		?>
 	</BODY> 
 </HTML> 
--- PHP/Ejercicios/ej6s.php
+++ PHP/Ejercicios/ej6s.php
@@ -5,13 +5,10 @@
 	<BODY> 
 <?php  
 	$num="48"; 
-	$base="8";
+	$base="16";
 	for($i=0;$i<=4;$i++){
-	echo "En $num en $base = ". base_convert($num, 10,$base)."<br>";
-	$base=$base-2;
-		if($base==0){
-			break;
-		}
+		echo "En $num en $base = ". base_convert($num, 10,$base)."<br>";
+		$num=$num/2;
 	}
 ?>
 	</BODY> 
