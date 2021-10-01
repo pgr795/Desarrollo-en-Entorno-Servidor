d5a96ee52ec6cc5d9ce12df221adbbdee47126e4
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-10-01 12:49:19 +0200

    Add files via upload

---
+++ PHP/Ejercicios/bolsa2.php
@@ -0,0 +1,25 @@
+<?php
+echo "<b>IBEX35
+</b>";echo "<br>";
+$ibex35=array(
+	"Acciona"=>array("Precio"=>31,"Variacion"=>"-0.06%","Var"=>"0.02€","Vol"=>478.240,"VolEuro"=>15.115
+	),
+	"ACERINOX"=>array("Precio"=>31,"Variacion"=>"-0.06%","Var"=>"0.02€","Vol"=>478.240,"VolEuro"=>15.115
+	),
+	"ACS"=>array("Precio"=>31,"Variacion"=>"-0.06%","Var"=>"0.02€","Vol"=>478.240,"VolEuro"=>15.115
+	),
+	"AENA"=>array("Precio"=>31,"Variacion"=>"-0.06%","Var"=>"0.02€","Vol"=>478.240,"VolEuro"=>15.115
+	),
+	"ACS"=>array("Precio"=>31,"Variacion"=>"-0.06%","Var"=>"0.02€","Vol"=>478.240,"VolEuro"=>15.115
+	),
+	"ALMIRALL"=>array("Precio"=>31,"Variacion"=>"-0.06%","Var"=>"0.02€","Vol"=>478.240,"VolEuro"=>15.115
+	),
+	"AMADEUS"=>array("Precio"=>31,"Variacion"=>"-0.06%","Var"=>"0.02€","Vol"=>478.240,"VolEuro"=>15.115
+	),
+	);
+
+
+		
+var_dump($ibex35);
+
+?>
