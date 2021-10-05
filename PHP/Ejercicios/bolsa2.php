7957d76f53e689d80a8e4a3afda125934f23b85c
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-10-05 09:02:24 +0200

    Add files via upload

--- PHP/Ejercicios/bolsa2.php
+++ PHP/Ejercicios/bolsa2.php
@@ -69,9 +69,16 @@ echo "<b>IBEX35
 	); */
 
 $Ibex35 = array();
+
 for ($i = 0; $i < 35; $i++){
-	$Ibex35[$i]=array("AUS$i"=>array("Precio"=>random_int(100, 999),"Variacion"=>random_int(100, 999),"Var"=>random_int(100, 999),"Vol"=>random_int(100, 999),"VolEuro"=>random_int(100, 999)
-	));
+	$nombre="Aus$i";
+	$Ibex35[$nombre]= array(
+	"Precio"=>random_int(100, 999),
+	"Variacion"=>random_int(100, 999),
+	"Var"=>random_int(100, 999),
+	"Vol"=>random_int(100, 999),
+	"VolEuro"=>random_int(100, 999)
+	);
 }
 		
 /* var_dump($ibex35); */
