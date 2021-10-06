dd0b38c27e5b22de00a9216bee32920c2a1e3837
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-10-06 09:57:59 +0200

    Add files via upload

--- JavaScript/practica8.js
+++ JavaScript/practica8.js
@@ -15,6 +15,8 @@ var especial="@";
 var valido=true;
 var longitud=texto.length;
 var posicion;
+var desparroba;
+var posicion;
 
 //Empezar con letra
 //Va a tener letras,digitos, - y .
@@ -24,23 +26,43 @@ var posicion;
 // .
 // letras entre 2 y 4
 	
-
-	
-	for(i=1;i<texto.length;i++){
-		var primeraLetra=texto[0];
-		var letras=texto[i];
-		posicion=i;
-		if(!minusculas.includes(primeraLetra) && !mayusculas.includes(primeraLetra))
-		{
-			valido=false;
-		}
-		if(!minusculas.includes(letras) && !mayusculas.includes(letras) 
-			&& !numeros.includes(letras) && !especiales.includes(letras) 
-			&& !especial.includes(letras) )
-		{
-			valido=false;
+/* var texto= "juanpablo";
+var correcto= true;
+if(!texto.includes("@"))
+    correcto= false;
+if(correcto) {
+    alert("bien");
+} else {
+    alert("mal");
+} */
+	if(!texto.includes("@")){
+		valido=false;
+	}
+	else{
+		for(i=1;i<texto.length;i++){
+			var primeraLetra=texto[0];
+			var letras=texto[i];
+			posicion=i;
+			if(!minusculas.includes(primeraLetra) && !mayusculas.includes(primeraLetra))
+			{
+				valido=false;
+			}
+			if(!minusculas.includes(letras) && !mayusculas.includes(letras) 
+				&& !numeros.includes(letras) && !especiales.includes(letras))
+			{
+				valido=false;
+			}
+			if(!letras.includes(especial)){
+				posicion=texto.indexOf(especial);
+				desparroba=texto[posicion+1];
+			}
+			if(!(desparroba=="a")){
+					valido=false;
+			}
 		}
 	}
+	
+	console.log(desparroba);
 	console.log(valido);
 	
 	if(valido){
