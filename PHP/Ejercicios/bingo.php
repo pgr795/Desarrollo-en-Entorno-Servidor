3aaf7617c10541f5f6e0847e6a3fe21621ae19cb
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-10-10 09:38:24 +0200

    Add files via upload

--- PHP/Ejercicios/bingo.php
+++ PHP/Ejercicios/bingo.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /* 4 jugadores
 3 cartones
 15 numeros cada uno
@@ -45,25 +45,46 @@ for($i=2; $i<61;$i++) {
 }
 
 //JUGAR
-$BolaExtraida=rand(1,60);
+$bolaExtraida=rand(1,60);
+echo $bolaExtraida."<br>";
 
-$
 //Jugador
+$tachados= array();
 if(in_array($bolaExtraida,$carton)){
-	
+	$tachados=$bolaExtraida;
+	echo $tachadoComprobacion."<br>";
+	$bola=array_search($bolaExtraida,$carton);
+	echo $bola."<br>";
+	$tachoDelcarton=$carton[$bola]=0;
+	echo $tachoDelcarton;
+	echo "acierto";
+}
+else if (!(in_array($bolaExtraida,$carton))){
+	echo "jodete";
 
 }
-
 //BOMBO
-if(in_array){
-	
+
+
+
+if(in_array($bolaExtraida,$bombo)){
+	$aux=$bombo[$bolaExtraida]=0;
+	echo $aux;
 	
 }
-
+else if($bombo[$bolaExtraida]==0){
+    $aux2=$bolaExtraida=rand(1,60);
+    echo $aux2;
+}
+	
+	
 
 //array_search — Busca un valor determinado en un array y devuelve la primera clave correspondiente en caso de éxito
 //array_replace — Reemplaza los elementos del array original con elementos de array adicionales 
+echo "<br>";
+echo "<br>";
 
+var_dump($tachados);
 var_dump($carton);
 var_dump($bombo);
 /* numeros ramdom del 1 al 60 */
