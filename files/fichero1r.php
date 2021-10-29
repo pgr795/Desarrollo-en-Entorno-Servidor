a7d00611d8a5cbde592b253ed41bf7c9f2bd5c23
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-10-29 13:31:50 +0200

    Add files via upload

--- files/fichero1r.php
+++ files/fichero1r.php
@@ -5,11 +5,26 @@ echo "<body>";
 $fichero2=file("alumnos1.txt");
 var_dump($fichero2);
 
+$aux=implode(" ",$fichero2);
+var_dump($aux);
+
+$contenido=explode(" ",$aux);
+$resultado = array_unique($contenido);
+
+var_dump($resultado);
+
 foreach($fichero2 as $linea=>$texto) {
 	echo "<table style='border: solid;'>".
 		 "<tr>".
-			"<td style='border: solid;'>"."Linea: ",$linea."</td>".
-			"<td style='border: solid;'>".$texto."</td>".
+			"<td>"."Nombre"."</td>".
+			"<td>"."Apellido1"."</td>".
+			"<td>"."Apellido2"."</td>".
+			"<td>"."Fecha"."</td>".
+			"<td>"."Localidad"."</td>".
+		 "</tr>".
+		 "<tr>".
+			"<td>"."Linea: ",$linea."</td>".
+			"<td>".$texto."</td>".
 		 "</tr>".
 		 "</table>";
 };
