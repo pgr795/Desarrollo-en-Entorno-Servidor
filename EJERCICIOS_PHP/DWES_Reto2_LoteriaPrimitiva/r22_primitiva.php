7614fe20546a4d8ef1948e9bd30147675686391f
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-11-14 02:04:45 +0100

    Add files via upload

--- EJERCICIOS_PHP/DWES_Reto2_LoteriaPrimitiva/r22_primitiva.php
+++ EJERCICIOS_PHP/DWES_Reto2_LoteriaPrimitiva/r22_primitiva.php
@@ -22,11 +22,34 @@
 		$valor2 = limpieza($_POST["recaudacion"]);
 		$valor3 = combinacionGanadora($_POST["combinacion"]);
 	}
+	
 	$fechaSorteo=$valor1;
 	$recaudacion=$valor2;
 	$combinacionGanadora=$valor3;
+	var_dump($combinacionGanadora);
 
+	$fichero="r22_primitiva.txt";
+	//Para mostrar el resultado de la combinacion Ganadora
 	echo mostrarResultado($combinacionGanadora,$fechaSorteo);
-	
 
+	//Con esta funcion extraeremos del fichero r_22primitiva.txt todos los numeros
+	//con un array de 100 posiciones donde se guardaran los numeros
+	
+	$numerosPrimitiva=numeros($fichero);
+
+	//Con esta funcion nos sacara un array de 100 posiciones con 4 indices
+	// [0] numeros acertados, [1] complementario, [2] reintegro, [3] fallos.  
+	$acertantes=Acertantes(numeros($fichero),$combinacionGanadora);
+
+	echo mostrarAcertantes($acertantes);
+
+	// var_dump($numerosPrimitiva);
+	var_dump($acertantes);
+
+
+//
+// $aux=clasificacionAcertantes($jug);
+// var_dump($aux);
+
+	
 ?>
