2c86d025e363277986caf7a887cf6f719ea3b98d
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2021-11-23 10:59:15 +0100

    Add files via upload

---
+++ EJERCICIOS_PHP/EjerciciosMySQL/Ejercicio2/alta_d.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Alta Departamento</title>
+</head>
+<body>
+	<h1>Alta Departamento</h1>
+	<form method="post" action='funciones.php'>
+		<p>Departamento <input type='text' name='departamento' size=15></p><br>
+		   <input type="submit" name="submit" value="Alta"/>
+	</form>
+</body>
+</html>
+<?php
+include('funciones.php');
+	/* var_dump($_POST); */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$valor1 = limpieza($_POST["departamento"]);
+	
+	$departamento=$valor1;
+
+	$conexion=crear_conexion2();
+}
+	var_dump($conexion);
+
+	alta_departamento($conexion,$departamento);
+	
+	cerrarConexion($conexion);
+
+?>
