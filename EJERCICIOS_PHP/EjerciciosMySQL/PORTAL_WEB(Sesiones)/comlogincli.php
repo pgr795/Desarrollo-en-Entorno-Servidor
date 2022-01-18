4ec2c5d23f1df9c44970601aa170d156d0cd52f9
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2022-01-18 10:23:41 +0100

    Add files via upload

---
+++ EJERCICIOS_PHP/EjerciciosMySQL/PORTAL_WEB(Sesiones)/comlogincli.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<?php
+	include('funciones.php');
+	var_dump($_POST);
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		try{
+			$usuario = limpieza($_POST["usuario"]);
+			$contraseña = limpieza($_POST["contraseña"]);
+		
+			$conexion=crear_conexion();
+			$stmt = $conexion->prepare("SELECT nombre,nif,clave FROM cliente where nif='$usuario' and clave='$contraseña' ");
+			$stmt->execute();
+			
+			$cont=0;
+			
+			$usuarioBD="";
+			$nifBD="";
+			
+			foreach($stmt->fetchAll() as $consulta){
+				$usuarioBD=$consulta["nombre"];
+				$nifBD=$consulta["nif"];
+				$cont++;
+			}
+			
+			if($cont == 1) {
+				session_start();
+				$_SESSION['nombre'] = $usuarioBD;
+				$_SESSION['nif'] = $nifBD;
+				header("location: comprocli.php");
+				
+			}
+			else { 
+				$error = "Usuario o password incorrectos !!!";
+				echo $error;
+			}
+		}
+		catch(PDOException $e){
+			echo "Error: " . $e->getMessage();	
+		}
+	}
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+</head>
+<body>
+	<h2>LOGIN</h2>
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+			<p>Usuario <input type='text' name='usuario' size=15></p>
+			<p>Contraseña<input type='text' name='contraseña' size=15></p>
+			<input type='submit' name='submit' value='Iniciar Sesion'/>
+		</form>
+</body>
+</html>
---
+++ EJERCICIOS_PHP/EjerciciosMySQL/PORTAL_WEB(Sesiones)/comprocli.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<?php
+	include('funciones.php');
+	var_dump($_POST);
+	
+	session_start();
+	var_dump($_SESSION);
+	
+	if (!isset($_SESSION['nif'])){
+		header("location: comlogincli.php");
+	}
+    	
+?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Registro</title>
+</head>
+<body>
+	<h1>Bienvenido <?php echo $_SESSION['nombre']."-NIF: ".$_SESSION['nif']; ?></h1> 
+	<h2>Alta de Registro</h2>
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+			<div>
+				<input type="submit" value="Comprar Productos" name="comprar">
+				<input type="submit" value="Consulta Compras" name="consultar">
+				<input type="submit" value="Cerrar Sesión" name="cerrar">
+			</div>	
+		</form>
+</body>
+</html>
+<?php
+// var_dump($_POST);
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$valor1 = limpieza($_POST["comprar"]);
+		$valor2 = limpieza($_POST["consultar"]);
+		$valor3 = limpieza($_POST["cerrar"]);
+		
+		
+		if($valor1){
+			header("location: compro.php");
+		}
+		else if($valor2){
+			header("location: comconscom.php");
+		}
+		else if($valor3){
+			// remove all session variables
+			session_unset();
+			// destroy the session
+			session_destroy();
+			header("location: comlogincli.php");
+		}
+	}
+?>
