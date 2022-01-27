85edab2255dbda3c89b9d31b4874dcc49ce9fa68
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2022-01-27 19:56:29 +0100

    Add files via upload

--- EJERCICIOS_PHP/Proyecto/Funciones/funciones.php
+++ EJERCICIOS_PHP/Proyecto/Funciones/funciones.php
@@ -1,24 +1,19 @@
 <?php
 
 function conexion(){
-
-  $servername = "localhost";
-  $username = "root";
-  $password = "rootroot";
-  $dbname = "pedidos";
-
-  try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-
-  catch(PDOException $e){
-    echo "Error: " . $e->getMessage();
-  }
-
-  return $conn;
-}//de function
+		$servername = "localhost"; //or IP
+		$username = "root";
+		$password = "rootroot";
+		$dbname="pedidos";
+		try {
+		$conexion = new PDO("mysql:host=$servername;dbname=pedidos",$username,$password);
+		$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		return $conexion;
+		}
+		catch(PDOException $e){
+			echo "Conexion fallida: " . $e->getMessage();
+		}
+}
 
 //--------------
 
@@ -30,38 +25,9 @@ function limpiarCampo($campo){
 }
 
 
-//--------------------------------------------------------------------------------
-
-function desplegableNumCliente($baseDatos){
-
-//la información de los pedidos realizados: número pedido (orderNumber), fecha pedido (orderDate), estado pedido (status).
-
-
-  $stmt = $baseDatos->prepare("SELECT orderNumber,orderDate,status FROM orders where ");
-  $stmt->execute();
-
-  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-  return $stmt;
-
-}//de function
-
 //----------------------------------------------------
 
-function desplegablePedidos($baseDatos){
-
-  $stmt = $baseDatos->prepare("SELECT customerNumber,customerName FROM customers");
-  $stmt->execute();
-
-  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-  return $stmt;
-
-}//de function
-
-//----------------------------------------------------
-
-function mostrarProductos(){
+function selectProductos(){
 		try {
 			$conexion=conexion();
 			$stmt = $conexion->prepare("SELECT productName,productCode FROM products where quantityInStock=quantityInStock>=0");
@@ -77,42 +43,29 @@ function mostrarProductos(){
 			echo "Error:". $e->getMessage();
 		}
 		$conexion = null;
+}
+
+//----------------------------------------------------
+function mostrarProductos($aux){
+	
+		$datos=$aux;
+		
+		foreach($datos as $indice => $valor){
+				echo "<p>Producto:$valor[0] Unidades:$valor[1]</p>";
+		}
+
 	}
 //----------------------------------------------------
+function mostrarPrecioTotal($PrecioTotal){
+	$aux=$_SESSION['datos'];
+	mostrarProductos($aux);
+	echo "<p>Precio total:$PrecioTotal</p>";
 
-	function generarCodigoOrders(){
+}
+//----------------------------------------------------
+function generarCodigoPedido(){
 		try {
-			$conexion=crear_conexion();
-			$stmt = $conexion -> prepare("SELECT MAX(orderNumber) as ultimoCodigo FROM orders");
-			$stmt->execute();
-			$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-			
-			foreach($stmt->fetchAll() as $row){
-				$ultimoCodigo=$row['ultimoCodigo'];
-			}
-		
-			if(empty($ultimoCodigo)){
-				$CodigoDepartamento=$auxCodigoDepartamento;
-			}
-			else{
-				$codigo="0";
-				$num=$codigo;
-				$num+=1;
-				$CodigoDepartamento=$num;
-			}
-			return $CodigoDepartamento;
-		}
-		catch(PDOException $e) {
-			echo "Error: ". $e->getMessage();
-		}
-		
-		$conexion = null;
-		
-	}
-	
-	function generarCodigoOrdersDetails(){
-		try {
-			$conexion=crear_conexion();
+			$conexion=conexion();
 			$stmt = $conexion -> prepare("SELECT MAX(orderNumber) as ultimoCodigo FROM orderdetails");
 			$stmt->execute();
 			$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
@@ -122,10 +75,10 @@ function mostrarProductos(){
 			}
 		
 			if(empty($ultimoCodigo)){
-				$CodigoDepartamento=$auxCodigoDepartamento;
+				$CodigoDepartamento=1;
 			}
 			else{
-				$codigo="0";
+				$codigo=$ultimoCodigo;
 				$num=$codigo;
 				$num+=1;
 				$CodigoDepartamento=$num;
@@ -136,26 +89,163 @@ function mostrarProductos(){
 			echo "Error: ". $e->getMessage();
 		}
 		$conexion = null;
-	}
-		
-
-	function insertarOrders($){
-		
+}
 	
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------
+//insertarPedido($conexion,$customerNumber,$productCode,$quantityOrdered,$checkNumber,$orderDate,$requiredDate,$shippedDate){
 	
+	//datosRecogidos=productCode,quantityOrdered,
+	
+function insertarPedido($conexion,$customerNumber,$datosRecogidos,$orderDate,$requiredDate,$shippedDate,$checkNumber){
+		try {
+			$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+			$orderNumber=generarCodigoPedido();//codigoPedido
+			
+			//var_dump($orderNumber);
+			
+			insertarOrders($conexion,$orderNumber,$orderDate,$requiredDate,$shippedDate,$customerNumber);
+			
+			insertarOrderDetails($conexion,$orderNumber,$datosRecogidos);//$productCode,$quantityOrdered
+						
+			insertPayments($conexion,$customerNumber,$orderNumber,$checkNumber,$datosRecogidos);//$productCode
+			
+			actualizarAlmacen($conexion,$datosRecogidos);//$productCode,$quantityOrdered
+			
+			$PrecioTotal=totalDelPedido($conexion,$orderNumber);
+			
+			mostrarPrecioTotal($PrecioTotal);
+		}
+		catch(PDOException $e) {
+			echo "Error: ". $e->getMessage();
+		}
+		$conexion = null;
+}
+	
+//Datos al insertar: orderNumber,orderDate,requiredDate,shippedDate("Null"),status,comments("null"),customerNumber	
+function insertarOrders($conexion,$orderNumber,$orderDate,$requiredDate,$shippedDate,$customerNumber){
+		try {
+			$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			$stmt = $conexion->prepare("INSERT INTO orders (orderNumber,orderDate,requiredDate,shippedDate,status,comments,customerNumber)
+			VALUES ('$orderNumber', '$orderDate', '$requiredDate', '$shippedDate', 'In Process', 'null','$customerNumber')");
+			//STATUS NO SE QUE HAY QUE PONER.
+			$stmt->execute();
+		}
+		
+		catch(PDOException $e) {
+			echo "Error al insertar Orders: ". $e->getMessage();
+		}
+		
+		$conexion = null;	
+}
+	
+
+//Datos al insertar: orderNumber,productCode,quantityOrdered,priceEach,orderLineNumber
+function insertarOrderDetails($conexion,$orderNumber,$datosRecogidos){
+		try {
+			$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+			foreach($datosRecogidos as $indice=> $valor){
+				$productCode=$valor[0];
+				$quantityOrdered=$valor[1];
+				$priceEach=buscarPrecio($productCode); //Precio del producto
+				$orderLineNumber=rand(1,32767);	//PENDIENTE DE MODIFICAR
+				$stmt = $conexion->prepare("INSERT INTO orderdetails (orderNumber,productCode,quantityOrdered,priceEach,orderLineNumber)
+				VALUES ('$orderNumber', '$productCode', '$quantityOrdered', '$priceEach', '$orderLineNumber')");
+				$stmt->execute();
+			}
+		}
+		catch(PDOException $e){
+				echo "Error al insertar OrderDetails: ". $e->getMessage();
+		}
+		$conexion = null;	
+}
+
+//customerNumber,checkNumber,paymentDate,amount
+function insertPayments($conexion,$customerNumber,$checkNumber,$orderNumber){
+		try{
+			$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$paymentdate=date("Y-m-d");
+			$amount=totalDelPedido($conexion,$orderNumber);
+			//var_dump($amount);
+			$stmt = $conexion->prepare("INSERT INTO payments (customerNumber,checkNumber,paymentDate,amount)
+			VALUES ('$customerNumber', '$checkNumber', '$paymentdate', '$amount')");
+			$stmt->execute();
+	
+		}
+		catch(PDOException $e){
+				echo "Error al insertar Payments: ". $e->getMessage();
+		}
+		$conexion = null;	
+}
+
+
+//Precio del Producto
+function buscarPrecio($productCode){
+		try {
+			$conexion=conexion();
+			$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+			$stmt = $conexion->prepare("SELECT msrp FROM products WHERE productCode='$productCode'");
+			$stmt->execute();
+			$priceEach=$stmt->setFetchMode(PDO::FETCH_ASSOC);
+			//var_dump($priceEach);
+			return $priceEach;
+		}
+		catch(PDOException $e){
+				echo "Error al buscar precio: ". $e->getMessage();
+			}
+		$conexion = null;	
 	}
 
+//ActualizarAlmacen
+function actualizarAlmacen($conexion,$datosRecogidos){
+	try {
+		$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+		foreach($datosRecogidos as $indice=> $valor){
+			$productCode=$valor[0];
+			$quantityOrdered=$valor[1];
+			$stmt = $conexion->prepare("UPDATE products SET quantityInStock=quantityInStock-$quantityOrdered WHERE productCode='$productCode'");
+			$stmt->execute();
+		}
+	}
+	catch(PDOException $e){
+			echo "Error al actualizar el Almacen: ". $e->getMessage();
+	}
+		$conexion = null;	
+}
 
 
+//----------------------------------------------------	
+//PRECIO TOTAL DEL PEDIDO
 
-
-
-
-
-
-
-
-
-
-
+function totalDelPedido($conexion,$orderNumber){
+	try {
+		$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//$stmt = $conexion->prepare("UPDATE products SET quantityInStock=quantityInStock-$quantityOrdered WHERE productCode='$productCode'");
+		//$stmt = $conexion->prepare("SELECT quantityordered, priceeach FROM orderdetails WHERE orderNumber='$orderNumber'");
+		
+		$stmt=$conexion->prepare("SELECT quantityOrdered,priceEach FROM orderdetails WHERE orderNumber='$orderNumber'");
+		$stmt->execute();
+	
+		$precio=0;
+	
+		foreach($stmt->fetchAll() as $consulta){
+			$precio+=floatval($consulta['quantityOrdered'])*floatval($consulta['priceEach']);
+			//var_dump($consulta['quantityOrdered']);
+			//var_dump($consulta['priceEach']);
+		}
+		return $precio;
+	}
+	catch(PDOException $e){
+			echo "Error al mostrar el precio total". $e->getMessage();;
+	}
+	$conexion=null;	
+}
+//select * from orders where customerNumber="495";
 ?>
--- EJERCICIOS_PHP/Proyecto/pe_altaped.php
+++ EJERCICIOS_PHP/Proyecto/pe_altaped.php
@@ -1,12 +1,10 @@
 <?php
 	include('./Funciones/funciones.php');
-	
 	session_start();
-	
 	if (!isset($_SESSION['customerNumber'])){
 		header("location: pe_login.php");
 	}
-	var_dump($_SESSION);
+	//var_dump($_SESSION);
 
 ?>
 <html>
@@ -19,13 +17,14 @@
 		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 			<p>Productos</p>
 			<?php
-				mostrarProductos();
+				selectProductos();
 			?>
 			<p>Cantidad <input type='text' name='cantidad' size=15></p>
 			<p>Forma de Pago/checkNumber <input type='text' name='pago' size=15></p>
 			<div>
+				<input type="submit" name="submit" value="Agregar Producto"/>
 				<input type="submit" name="submit" value="Alta Pedido"/>
-				<input type="submit" name="submit" value="Cerrar Sesion"/>
+				<input type="submit" name="submit" value="Borrar"/>
 			</div>
 		</form>
 </body>
@@ -36,24 +35,65 @@
 		$valor1 = limpiarCampo($_POST["productos"]);
 		$valor2 = limpiarCampo($_POST["cantidad"]);
 		$valor3 = limpiarCampo($_POST["pago"]);
-	var_dump($_POST);
+		
+	//var_dump($_POST);
 	
+	
+	$accion=$_POST["submit"];
 	
 	$conexion=conexion();
+	$customerNumber=$_SESSION['customerNumber'];//cliente
+	$productCode=$valor1;//productos
+	$quantityOrdered=$valor2;//cantidad
+	$checkNumber=$valor3; //Forma de pago
+    $orderDate=date("Y-m-d"); //fechaPedido
+    $requiredDate= date("Y-m-d"); //fechaSolicitud
+	$shippedDate= date("Y-m-d"); //fechaEnvio
 	
-	$productos=$valor1;
-	$cantidad=$valor2;
-	$pago=$valor3; //checkNumber
-	
-    $fechaPedido=date("Y-m-d"); //orderDate
-    $fechaSolicitud=date("Y-m-d");//requiredDAte
-	$fechaEnvio=null; //shippedDate
-    //$amount
-	
-	//GenerarCodigo
-	//ActualizarAlmacen
-	//InsertarPedido
-	//MostrarInformacionPago
-    
+	$datos=array();
+
+				if($accion=="Agregar Producto"){
+					if(!isset($_SESSION['datos'])){
+						if($quantityOrdered!=""){
+						//var_dump($unidades);
+							$datos=array(array($productCode,$quantityOrdered));
+							//var_dump($datos);
+							$_SESSION['datos']=$datos;
+							$aux=$_SESSION['datos'];
+							mostrarProductos($aux);
+						}
+						else{
+							echo "No has seleccionado unidades";
+							}
+					}
+					else{
+						if($quantityOrdered!=""){
+							$datos=array($productCode,$quantityOrdered);
+							array_push($_SESSION['datos'],$datos);
+							$aux=$_SESSION['datos'];
+							mostrarProductos($aux);
+						}
+						else{
+							echo "No has seleccionado unidades";
+						}
+					}
+					//var_dump($datos);
+					//var_dump($_SESSION['datos']);
+				}
+				else if($accion=="Alta Pedido"){
+					$datosRecogidos=$_SESSION['datos'];
+						
+					//insertarPedido($conexion,$customerNumber,$productCode,$quantityOrdered,$checkNumber,$orderDate,$requiredDate,$shippedDate);
+					insertarPedido($conexion,$customerNumber,$datosRecogidos,$orderDate,$requiredDate,$shippedDate,$checkNumber);
+					//header("location: pe_login");
+				}
+				else if($accion=="Borrar"){
+					unset($_SESSION['datos']);
+				}
+
    }
+	
+
+
+    
 ?>
