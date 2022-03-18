9f6b06840afe1f569e73c4aa568285abcf608994
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2022-03-18 23:47:26 +0100

    Delete WebJuegosAzar directory

--- WebJuegosAzar/controllers/Hacienda_controllers.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'db/db.php';
-include_once 'models/Hacienda_model.php';
-include_once 'views/Hacienda_views.php';
-
-var_dump(__file__);
-?>
--- WebJuegosAzar/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once 'db/db.php';
-include_once 'controllers/Hacienda_controllers.php';
-?>
